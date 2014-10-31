1eceba17df33c6ae06fd78a175aa4e90ddc68c12
Josueemx <Morales@MacBook-de-Josue.local>
2014-10-30 19:26:00 -0700

    katas

--- gameOfLifeKata.py
+++ gameOfLifeKata.py
@@ -1,17 +1,156 @@
+def vecinos(x, b):
+    global universo
+    global columnas
+    global lineas
+    b = universo
+    count = 0
+    if x == 0:
+        if universo[x+1]== "*":
+            count += 1
+        if universo[x+1+columnas] == "*":
+            count += 1
+        if universo[x+columnas] == "*":
+            count += 1
+        return(count)
+    if x == columnas-1:
+        if universo[x-1] == "*":
+            count += 1
+        if universo[x-1+columnas] == "*":
+            count += 1
+        if universo[x+columnas] == "*":
+            count += 1
+        return(count)
+    if x == (lineas-1)*columnas:
+        if universo[x-columnas] == "*":
+            count += 1
+        if universo[x+1-columnas] == "*":
+            count += 1
+        if universo[x+1]== "*":
+            count += 1
+        return(count)
+    if x == (lineas*columnas)-1:
+        if universo[x-columnas] == "*":
+            count += 1
+        if universo[x-1-columnas] == "*":
+            count += 1
+        if universo[x-1] == "*":
+            count += 1
+        return(count)
+    if x > (lineas-1)*columnas and x < (lineas*columnas)-1 :
+        if universo[x-1] == "*":
+            count += 1
+        if universo[x+1]== "*":
+            count += 1
+        if universo[x-1-columnas] == "*":
+            count += 1
+        if universo[x-columnas] == "*":
+            count += 1
+        if universo[x+1-columnas] == "*":
+            count += 1
+        return(count)
+    if (x+1) % columnas == 0:
+        if universo[x+columnas] == "*":
+            count += 1
+        if universo[x-1+columnas] == "*":
+            count += 1
+        if universo[x-1] == "*":
+            count += 1
+        if universo[x-columnas] == "*":
+            count += 1
+        if universo[x-1-columnas] == "*":
+            count += 1
+        return(count)
+    if x % columnas == 0:
+        if universo[x+1+columnas] == "*":
+            count += 1
+        if universo[x+columnas] == "*":
+            count += 1
+        if universo[x+1] == "*":
+            count += 1
+        if universo[x+1-columnas] == "*":
+            count += 1
+        if universo[x-columnas] == "*":
+            count += 1
+        return(count)
+    if x > 0 and x < columnas-1:
+        if universo[x+1+columnas] == "*":
+            count += 1
+        if universo[x+columnas] == "*":
+            count += 1
+        if universo[x-1+columnas] == "*":
+            count += 1
+        if universo[x+1] == "*":
+            count += 1
+        if universo[x-1] == "*":
+            count += 1
+        return(count)
+    if universo[x+1+columnas] == "*":
+        count += 1
+    if universo[x+columnas] == "*":
+        count += 1
+    if universo[x-1+columnas] == "*":
+        count += 1
+    if universo[x+1] == "*":
+        count += 1
+    if universo[x-1] == "*":
+        count += 1
+    if universo[x+1-columnas] == "*":
+        count += 1
+    if universo[x-columnas] == "*":
+        count += 1
+    if universo[x-1-columnas] == "*":
+        count += 1
+    return(count)
+
 def NextOfspring(u):
-    """
-        Esta funcion recibe un universo y regresa la siguiente generacion segun las reglas
-        Nota: La funcion recibe la cadena el el formato establecido y lo regresa en el mismo formato
-    """
-    raise NotImplementedError
+    global universo
+    global universo2
+    global columnas
+    global lineas
+    global cortar
+    cortar = "\n"
+    lineas = int(u[0])
+    columnas = int(u[1])
+    universo = u[2:]
+    xx = ""
+    universo2 = ""
+    for i in range(len(universo)):
+        if universo[i] == cortar:
+            ()
+        else:
+            xx += universo[i]
+    for a in range(len(universo)):
+        if universo[a] == "*":
+            if vecinos(a, universo) < 2:
+                universo2 += "."
+            elif vecinos(a, universo) > 3:
+                universo2 += "."
+            elif vecinos(a, universo) == 3 and vecinos(a, universo) == 2:
+                universo2 += "*"
+            else:
+                universo2 += "*"
+        elif universo[a] == "." and vecinos(a, universo) == 3:
+            universo2 += "*"
+        else:
+            universo2 += "."
+    c = str(lineas)+str(columnas) + universo2
+    return(c)
 
 def GetUniverse(u):
-    """
-        Esta funcion debe de regresar el universo en el formato establecido 
-        pero regresa el universo sin el renglon de las dimenciones y con los saltos
-        de linea correspondientes
-    """
-    raise NotImplementedError
+    global columnas
+    global lineas
+    global cortar
+    cortar = "\n"
+    lineas = int(u[0])
+    columnas = int(u[1])
+    uni = u[2:]
+    xx = ""
+    for i in range(lineas):
+        if i == lineas-1:
+            xx += uni[i*columnas:(i*columnas)+columnas]
+        else:
+            xx += uni[i*columnas:(i*columnas)+columnas] + cortar
+    return(xx + "\n")       
 
 if __name__ == '__main__':
     print("Captura un universo (Linea por linea segun el formato y vacio para terminar)")
--- gatoKata.py
+++ gatoKata.py
@@ -2,15 +2,15 @@
 template = " 1 │ 2 │ 3 \n───┼───┼───\n 4 │ 5 │ 6 \n───┼───┼───\n 7 │ 8 │ 9 "
 
 def GetTablero():
-    global template
-    return str(template)
+    return (str(template))
 
 def JuegoContinua():
-    global tie
-    return not tie
+    if (template[1] == "X" or template[1] == "O") and (template[5] == "X" or template[5] == "O") and (template[9] == "X" or template[9] == "O") and (template[25] == "X" or template[25] == "O") and (template[29] == "X" or template[29] == "O") and (template[33] == "X" or template[33] == "O") and (template[49] == "X" or template[49] == "O") and (template[53] == "X" or template[53] == "O") and (template[57] == "X" or template[57] == "O"):
+        return(False)
+    else:
+        return(True)
 
 def IntentarTirada(casilla):
-    global tie
     global template
     global turno
     global caserror
@@ -39,31 +39,22 @@ def IntentarTirada(casilla):
                     temp += template[x]
             template = temp
             if template[1] + template[5] + template[9] == turno * 3:
-                tie = True
                 return "Felicidades " + turno + " as ganado. weeee"
             if template[25] + template[29] + template[33] == turno * 3:
-                tie = True
                 return "Felicidades " + turno + " as ganado. weeee"
             if template[49] + template[53] + template[57] == turno * 3:
-                tie = True
                 return "Felicidades " + turno + " as ganado. weeee"
             if template[1] + template[25] + template[49] == turno * 3:
-                tie = True
                 return "Felicidades " + turno + " as ganado. weeee"
             if template[5] + template[29] + template[53] == turno * 3:
-                tie = True
                 return "Felicidades " + turno + " as ganado. weeee"
             if template[9] + template[33] + template[57] == turno * 3:
-                tie = True
                 return "Felicidades " + turno + " as ganado. weeee"
             if template[1] + template[29] + template[57] == turno * 3:
-                tie = True
                 return "Felicidades " + turno + " as ganado. weeee"
             if template[9] + template[29] + template[57] == turno * 3:
-                tie = True
                 return "Felicidades " + turno + " as ganado. weeee"
             if count > 8:
-                tie = True
                 return "Juego empatado. :("
     if casilla == 2:
         if template[5] == 'X' or template[5] == 'O':
@@ -80,31 +71,22 @@ def IntentarTirada(casilla):
                     temp += template[x]
             template = temp
             if template[1] + template[5] + template[9] == turno * 3:
-                tie = True
                 return "Felicidades " + turno + " as ganado. weeee"
             if template[25] + template[29] + template[33] == turno * 3:
-                tie = True
                 return "Felicidades " + turno + " as ganado. weeee"
             if template[49] + template[53] + template[57] == turno * 3:
-                tie = True
                 return "Felicidades " + turno + " as ganado. weeee"
             if template[1] + template[25] + template[49] == turno * 3:
-                tie = True
                 return "Felicidades " + turno + " as ganado. weeee"
             if template[5] + template[29] + template[53] == turno * 3:
-                tie = True
                 return "Felicidades " + turno + " as ganado. weeee"
             if template[9] + template[33] + template[57] == turno * 3:
-                tie = True
                 return "Felicidades " + turno + " as ganado. weeee"
             if template[1] + template[29] + template[57] == turno * 3:
-                tie = True
                 return "Felicidades " + turno + " as ganado. weeee"
             if template[9] + template[29] + template[57] == turno * 3:
-                tie = True
                 return "Felicidades " + turno + " as ganado. weeee"
             if count > 8:
-                tie = True
                 return "Juego empatado. :("
     if casilla == 3:
         if template[9] == 'X' or template[9] == 'O':
@@ -121,31 +103,22 @@ def IntentarTirada(casilla):
                     temp += template[x]
             template = temp
             if template[1] + template[5] + template[9] == turno * 3:
-                tie = True
                 return "Felicidades " + turno + " as ganado. weeee"
             if template[25] + template[29] + template[33] == turno * 3:
-                tie = True
                 return "Felicidades " + turno + " as ganado. weeee"
             if template[49] + template[53] + template[57] == turno * 3:
-                tie = True
                 return "Felicidades " + turno + " as ganado. weeee"
             if template[1] + template[25] + template[49] == turno * 3:
-                tie = True
                 return "Felicidades " + turno + " as ganado. weeee"
             if template[5] + template[29] + template[53] == turno * 3:
-                tie = True
                 return "Felicidades " + turno + " as ganado. weeee"
             if template[9] + template[33] + template[57] == turno * 3:
-                tie = True
                 return "Felicidades " + turno + " as ganado. weeee"
             if template[1] + template[29] + template[57] == turno * 3:
-                tie = True
                 return "Felicidades " + turno + " as ganado. weeee"
             if template[9] + template[29] + template[49] == turno * 3:
-                tie = True
                 return "Felicidades " + turno + " as ganado. weeee"
             if count > 8:
-                tie = True
                 return "Juego empatado. :("
     if casilla == 4:
         if template[25] == 'X' or template[25] == 'O':
@@ -162,31 +135,22 @@ def IntentarTirada(casilla):
                     temp += template[x]
             template = temp
             if template[1] + template[5] + template[9] == turno * 3:
-                tie = True
                 return "Felicidades " + turno + " as ganado. weeee"
             if template[25] + template[29] + template[33] == turno * 3:
-                tie = True
                 return "Felicidades " + turno + " as ganado. weeee"
             if template[49] + template[53] + template[57] == turno * 3:
-                tie = True
                 return "Felicidades " + turno + " as ganado. weeee"
             if template[1] + template[25] + template[49] == turno * 3:
-                tie = True
                 return "Felicidades " + turno + " as ganado. weeee"
             if template[5] + template[29] + template[53] == turno * 3:
-                tie = True
                 return "Felicidades " + turno + " as ganado. weeee"
             if template[9] + template[33] + template[57] == turno * 3:
-                tie = True
                 return "Felicidades " + turno + " as ganado. weeee"
             if template[1] + template[29] + template[57] == turno * 3:
-                tie = True
                 return "Felicidades " + turno + " as ganado. weeee"
             if template[9] + template[29] + template[57] == turno * 3:
-                tie = True
                 return "Felicidades " + turno + " as ganado. weeee"
             if count > 8:
-                tie = True
                 return "Juego empatado. :("
     if casilla == 5:
         if template[29] == 'X' or template[29] == 'O':
@@ -203,31 +167,22 @@ def IntentarTirada(casilla):
                     temp += template[x]
             template = temp
             if template[1] + template[5] + template[9] == turno * 3:
-                tie = True
                 return "Felicidades " + turno + " as ganado. weeee"
             if template[25] + template[29] + template[33] == turno * 3:
-                tie = True
                 return "Felicidades " + turno + " as ganado. weeee"
             if template[49] + template[53] + template[57] == turno * 3:
-                tie = True
                 return "Felicidades " + turno + " as ganado. weeee"
             if template[1] + template[25] + template[49] == turno * 3:
-                tie = True
                 return "Felicidades " + turno + " as ganado. weeee"
             if template[5] + template[29] + template[53] == turno * 3:
-                tie = True
                 return "Felicidades " + turno + " as ganado. weeee"
             if template[9] + template[33] + template[57] == turno * 3:
-                tie = True
                 return "Felicidades " + turno + " as ganado. weeee"
             if template[1] + template[29] + template[57] == turno * 3:
-                tie = True
                 return "Felicidades " + turno + " as ganado. weeee"
             if template[9] + template[29] + template[57] == turno * 3:
-                tie = True
                 return "Felicidades " + turno + " as ganado. weeee"
             if count > 8:
-                tie = True
                 return "Juego empatado. :("
     if casilla == 6:
         if template[33] == 'X' or template[33] == 'O':
@@ -244,31 +199,22 @@ def IntentarTirada(casilla):
                     temp += template[x]
             template = temp
             if template[1] + template[5] + template[9] == turno * 3:
-                tie = True
                 return "Felicidades " + turno + " as ganado. weeee"
             if template[25] + template[29] + template[33] == turno * 3:
-                tie = True
                 return "Felicidades " + turno + " as ganado. weeee"
             if template[49] + template[53] + template[57] == turno * 3:
-                tie = True
                 return "Felicidades " + turno + " as ganado. weeee"
             if template[1] + template[25] + template[49] == turno * 3:
-                tie = True
                 return "Felicidades " + turno + " as ganado. weeee"
             if template[5] + template[29] + template[53] == turno * 3:
-                tie = True
                 return "Felicidades " + turno + " as ganado. weeee"
             if template[9] + template[33] + template[57] == turno * 3:
-                tie = True
                 return "Felicidades " + turno + " as ganado. weeee"
             if template[1] + template[29] + template[57] == turno * 3:
-                tie = True
                 return "Felicidades " + turno + " as ganado. weeee"
             if template[9] + template[29] + template[57] == turno * 3:
-                tie = True
                 return "Felicidades " + turno + " as ganado. weeee"
             if count > 8:
-                tie = True
                 return "Juego empatado. :("
     if casilla == 7:
         if template[49] == 'X' or template[49] == 'O':
@@ -285,31 +231,22 @@ def IntentarTirada(casilla):
                     temp += template[x]
             template = temp
             if template[1] + template[5] + template[9] == turno * 3:
-                tie = True
                 return "Felicidades " + turno + " as ganado. weeee"
             if template[25] + template[29] + template[33] == turno * 3:
-                tie = True
                 return "Felicidades " + turno + " as ganado. weeee"
             if template[49] + template[53] + template[57] == turno * 3:
-                tie = True
                 return "Felicidades " + turno + " as ganado. weeee"
             if template[1] + template[25] + template[49] == turno * 3:
-                tie = True
                 return "Felicidades " + turno + " as ganado. weeee"
             if template[5] + template[29] + template[53] == turno * 3:
-                tie = True
                 return "Felicidades " + turno + " as ganado. weeee"
             if template[9] + template[33] + template[57] == turno * 3:
-                tie = True
                 return "Felicidades " + turno + " as ganado. weeee"
             if template[1] + template[29] + template[57] == turno * 3:
-                tie = True
                 return "Felicidades " + turno + " as ganado. weeee"
             if template[9] + template[29] + template[57] == turno * 3:
-                tie = True
                 return "Felicidades " + turno + " as ganado. weeee"
             if count > 8:
-                tie = True
                 return "Juego empatado. :("
     if casilla == 8:
         if template[53] == 'X' or template[53] == 'O':
@@ -326,31 +263,22 @@ def IntentarTirada(casilla):
                     temp += template[x]
             template = temp
             if template[1] + template[5] + template[9] == turno * 3:
-                tie = True
                 return "Felicidades " + turno + " as ganado. weeee"
             if template[25] + template[29] + template[33] == turno * 3:
-                tie = True
                 return "Felicidades " + turno + " as ganado. weeee"
             if template[49] + template[53] + template[57] == turno * 3:
-                tie = True
                 return "Felicidades " + turno + " as ganado. weeee"
             if template[1] + template[25] + template[49] == turno * 3:
-                tie = True
                 return "Felicidades " + turno + " as ganado. weeee"
             if template[5] + template[29] + template[53] == turno * 3:
-                tie = True
                 return "Felicidades " + turno + " as ganado. weeee"
             if template[9] + template[33] + template[57] == turno * 3:
-                tie = True
                 return "Felicidades " + turno + " as ganado. weeee"
             if template[1] + template[29] + template[57] == turno * 3:
-                tie = True
                 return "Felicidades " + turno + " as ganado. weeee"
             if template[9] + template[29] + template[57] == turno * 3:
-                tie = True
                 return "Felicidades " + turno + " as ganado. weeee"
             if count > 8:
-                tie = True
                 return "Juego empatado. :("
     if casilla == 9:
         if template[57] == 'X' or template[57] == 'O':
@@ -367,55 +295,34 @@ def IntentarTirada(casilla):
                     temp += template[x]
             template = temp
             if template[1] + template[5] + template[9] == turno * 3:
-                tie = True
                 return "Felicidades " + turno + " as ganado. weeee"
             if template[25] + template[29] + template[33] == turno * 3:
-                tie = True
                 return "Felicidades " + turno + " as ganado. weeee"
             if template[49] + template[53] + template[57] == turno * 3:
-                tie = True
                 return "Felicidades " + turno + " as ganado. weeee"
             if template[1] + template[25] + template[49] == turno * 3:
-                tie = True
                 return "Felicidades " + turno + " as ganado. weeee"
             if template[5] + template[29] + template[53] == turno * 3:
-                tie = True
                 return "Felicidades " + turno + " as ganado. weeee"
             if template[9] + template[33] + template[57] == turno * 3:
-                tie = True
                 return "Felicidades " + turno + " as ganado. weeee"
             if template[1] + template[29] + template[57] == turno * 3:
-                tie = True
                 return "Felicidades " + turno + " as ganado. weeee"
             if template[9] + template[29] + template[57] == turno * 3:
-                tie = True
                 return "Felicidades " + turno + " as ganado. weeee"
             if count > 8:
-                tie = True
                 return "Juego empatado. :("
     return ""
-    """
-        Esta funcion recibe el intento del jugador por ocupar una casilla
-        y regresa una cadena segun los siguientes criterios:
-        Si esta fuera de rango: "La tirada debe de estar entre 1 y 9"
-        Si la casilla esta ocupada: "La casilla ya esta ocupada"
-        Si x a ganado: "Felicidades X as ganado. weeee"
-        Si o a ganado: "Felicidades O as ganado. weeee"
-        Si el juego a quedado empatado: "Juego empatado. :("
-        Ninguna de las anteriores: "" (cadena vacia)
-    """
 
 def IniciaJuego():
+    global template
     global turno
     global caserror
     global count
-    global tie
-    global template
     count = 0
     caserror = False
     turno = 'O'
     template = " 1 │ 2 │ 3 \n───┼───┼───\n 4 │ 5 │ 6 \n───┼───┼───\n 7 │ 8 │ 9 "
-    tie = False
 
 if __name__ == '__main__':
     IniciaJuego() 
